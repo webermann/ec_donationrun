341107b5e842d45417de88a5de7dc576b3598952
Hauke Webermann <hauke@webermann.net>
2013-12-31 17:50:33 +0000

    move to donationrun

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ec_donationrun".
+ *
+ * Auto generated 08-12-2013 15:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Donation Run',
+	'description' => '',
+	'category' => 'Frontend-Plug-Ins',
+	'shy' => 0,
+	'version' => '0.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Hauke Webermann',
+	'author_email' => 'hauke@webermann.net',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
+			'extbase' => '1.3.0-0.0.0',
+			'fluid' => '1.3.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
