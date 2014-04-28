e962588db42497899cbe0095613924b2688c0b67
Hauke Webermann <hauke@webermann.net>
2014-04-28 20:07:41 +0000

    Updated BackendController

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -26,70 +26,125 @@
 
 
 
-	/**
-	 *
-	 * Controller class for the backend module. Provides actions for listing all projects
-	 * in the TYPO3 backend.
-	 *
-	 * @author     Hauke Webermann <hauke@webermann.net>
-	 * @package    EcDonationrun
-	 * @subpackage Controller
-	 * @version    $Id$
-	 * @license    GNU Public License, version 2
-	 *             http://opensource.org/licenses/gpl-license.php
-	 *
-	 */
+/**
+ *
+ * Controller class for the backend module. Provides actions for listing all projects
+ * in the TYPO3 backend.
+ *
+ * @author     Hauke Webermann <hauke@webermann.net>
+ * @package    EcDonationrun
+ * @subpackage Controller
+ * @version    $Id$
+ * @license    GNU Public License, version 2
+ *             http://opensource.org/licenses/gpl-license.php
+ *
+ */
 
 Class Tx_EcDonationrun_Controller_BackendController Extends Tx_Extbase_MVC_Controller_ActionController {
 
-		/*
-		 * ATTRIBUTES
-		 */
+	/*
+	 * ATTRIBUTES
+	 */
+	/**
+	 * A run repository instance
+	 * @var Tx_EcDonationrun_Domain_Repository_RunRepository
+	 */
+	//Protected $runRepository;
+	/**
+	* A registration repository.
+	* @var Tx_EcDonationrun_Domain_Repository_RegistrationRepository
+	*/
+	//Protected $registrationRepository;
 
-		 /**
-		  * A registration repository.
-		  * @var Tx_EcDonationrun_Domain_Repository_RegistrationRepository
-		  */
-	Protected $registrationRepository;
-	
-		 /**
-		  * A donatoin repository.
-		  * @var Tx_EcDonationrun_Domain_Repository_DonationRepository
-		  */
-	Protected $donatoinRepository;
+	/**
+	 * A donatoin repository.
+	 * @var Tx_EcDonationrun_Domain_Repository_DonationRepository
+	 */
+	Protected $donationRepository;
 
-		/*
-		 * ACTION METHODS
-		 */
+	/**
+	 * A FE User repository instance
+	 * @var Tx_EcAssociation_Domain_Repository_UserRepository
+	 */
+	//protected $frontendUserRepository;
+	/**
+	* A FE User Group repository instance
+	* @var Tx_Extbase_Domain_Repository_FrontendUserGroupRepository
+	*/
+	//protected $frontendUserGroupRepository;
 
-		/**
-		 *
-		 * The initialization action. This methods creates instances of all required
-		 * repositories.
-		 *
-		 * @return void
-		 *
-		 */
+	/*
+	 * ACTION METHODS
+	 */
 
+	/**
+	 *
+	 * The initialization action. This methods creates instances of all required
+	 * repositories.
+	 *
+	 * @return void
+	 *
+	 */
 	Protected Function initializeAction() {
-		$this->registrationRepository =& t3lib_div::makeInstance('Tx_EcDonationrun_Domain_Repository_RegistrationRepository');
-		$this->donatoinRepository     =& t3lib_div::makeInstance('Tx_EcDonationrun_Domain_Repository_DonationRepository');
+		//$this->runRepository          =& t3lib_div::makeInstance('Tx_EcDonationrun_Domain_Repository_RunRepository');
+		//$this->registrationRepository =& t3lib_div::makeInstance('Tx_EcDonationrun_Domain_Repository_RegistrationRepository');
+		$this->donationRepository     =& t3lib_div::makeInstance('Tx_EcDonationrun_Domain_Repository_DonationRepository');
+		//$this->userRepository         =& t3lib_div::makeInstance('Tx_EcAssociation_Domain_Repository_UserRepository');
+		//$this->frontendUserGroupRepository =& t3lib_div::makeInstance('Tx_Extbase_Domain_Repository_FrontendUserGroupRepository');
 	}
 
 
 
-		/**
-		 *
-		 * The index action. Displays a list of all available registrations.
-		 *
-		 * @return void
-		 *
-		 */
-
+	/**
+	 * The index action. Displays a list of all available registrations.
+	 * @return void
+	 */
 	Public Function indexAction() {
-		$this->view->assign('registrations', $this->registrationRepository->findAll());
-		$this->view->assign('donation', $this->donatoinRepository->findAll());
+		$this->view->assign('donations', $this->donationRepository->findAllNoInvoice());
 	}
+
+	/**
+	 * The sendDonationRequest action.
+	 * @return void
+	 */
+	Public Function sendDonationRequestAction() {
+		if (!isset($this->settings['invoicePath'])) throw new Exception('EC Donationrun: invoicePath not set');
+
+/*
+		$donations = $this->donationRepository->findAllNoInvoice();
+
+		foreach ($donations as $donation) {
+				
+				
+				
+				
+
+				
+				
+		}
+		//Tx_EcDonationrun_Utility_Invoice::generateInvoice();
+
+		if (!is_file($this->settings['invoicePath']."test_dokument.pdf")) {
+			throw new Exception('EC Donationrun: Invoice does not exsit.');
+		}
+
+		Tx_EcDonationrun_Utility_SendMail::sendMail(
+		//array($donation->getUser()->getEmail() => $donation->getUser()->getName()),
+				'hauke@webermann.net',
+				"Running for Jesus - Spendenaufforderung",
+		//"Hallo ".$donation->getUser()->getName().",\n".
+
+				"herzlichen Dank für deine Spende bei Running for Jesus",
+		$this->settings['invoicePath']."test_dokument.pdf");
+
+*/
+
+	}
+
+
+
+
+
 
 }
 
