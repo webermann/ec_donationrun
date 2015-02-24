274306ef0a4f5d83f3d607891833b004904b5022
Hauke Webermann <hauke@webermann.net>
2015-02-24 15:49:11 +0000

    Added new fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,6 +6,12 @@ CREATE TABLE tx_ecdonationrun_domain_model_event (
 	city tinytext,
 	info text,
 	website tinytext,
+	donation_info text,
+	bank_account text,
+	contact_person tinytext
+	contact_person_mail tinytext,
+	invoice_number_format tinytext,
+	
 	runs int(11) unsigned DEFAULT '0', 
 	
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
