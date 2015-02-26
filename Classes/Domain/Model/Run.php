529f2b6230a9e0e39cf984da7b9c78b65257c93e
Hauke Webermann <hauke@webermann.net>
2015-02-26 15:07:36 +0000

    Added new distanceUnit

--- Classes/Domain/Model/Run.php
+++ Classes/Domain/Model/Run.php
@@ -26,117 +26,143 @@
 
 
 
-	/**
-	 *
-	 * A run.
-	 *
-	 * @author     Hauke Webermann <hauke@webermann.net>
-	 * @package    EcDonationrun
-	 * @subpackage Domain_Model
-	 * @version    $Id$
-	 * @license    GNU Public License, version 2
-	 *             http://opensource.org/licenses/gpl-license.php
-	 * @entity
-	 *
-	 */
+/**
+ *
+ * A run.
+ *
+ * @author     Hauke Webermann <hauke@webermann.net>
+ * @package    EcDonationrun
+ * @subpackage Domain_Model
+ * @version    $Id$
+ * @license    GNU Public License, version 2
+ *             http://opensource.org/licenses/gpl-license.php
+ * @entity
+ *
+ */
 
 class Tx_EcDonationrun_Domain_Model_Run extends Tx_Extbase_DomainObject_AbstractEntity {
 
-		/*
-		 * ATTRIBUTES
-		 */
-	
-		/**
-		 * The run name
-		 * @var string
-		 * @validate StringLength(minimum = 3, maximum = 255)
-		 */
+	/*
+	 * ATTRIBUTES
+	 */
+
+	/**
+	 * The run name
+	 * @var string
+	 * @validate StringLength(minimum = 3, maximum = 255)
+	 */
 	protected $name;
 
-		/**
-		 * The start time.
-		 * @var DateTime
-		 */
+	/**
+	 * The start time.
+	 * @var DateTime
+	 */
 	protected $start;
-	
-		/**
-		 * The run distance.
-		 * @var double
-		 */
+
+	/**
+	 * The run distance.
+	 * @var double
+	 */
 	protected $distance;
-	
+
+	/**
+	 * The run distance unit
+	 * @var int
+	 */
+	protected $distanceUnit;
+
 	/**
 	 * The event
 	 * @var Tx_EcDonationrun_Domain_Model_Event
 	 * @lazy
 	 */
 	protected $event=NULL;
-	
-		/**
-		 * A list of all registrations that are assigned to this run
-		 * @var Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Registration>
-		 * @lazy
-		 */
+
+	/**
+	 * A list of all registrations that are assigned to this run
+	 * @var Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Registration>
+	 * @lazy
+	 */
 	protected $registrations = NULL;
-	
-		/**
-		 * A list of all donations that are assigned to this run
-		 * @var Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Donation>
-		 * @lazy
-		 */
+
+	/**
+	 * A list of all donations that are assigned to this run
+	 * @var Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Donation>
+	 * @lazy
+	 */
 	protected $donations = NULL;
 
 
-		/*
-		 * CONSTRUCTOR
-		 */
+	/*
+	 * CONSTRUCTOR
+	 */
 
 
-		 /**
-		  *
-		  * Creates a new run. All arguments are optional, since every model class
-		  * has to implement an empty constructor.
-		  *
-		  *
-		  */
+	/**
+	 *
+	 * Creates a new run. All arguments are optional, since every model class
+	 * has to implement an empty constructor.
+	 *
+	 *
+	 */
 
 	public function __construct () {
 
 	}
 
-		/*
-		 * GETTERS
-		 */
+	/*
+	 * GETTERS
+	 */
 
-		 /**
-		  *
-		  * Gets the run name
-		  * @return string The run name
-		  *
-		  */
+	/**
+	 *
+	 * Gets the run name
+	 * @return string The run name
+	 *
+	 */
 	public function getName() {
 		return $this->name;
 	}
 
-		 /**
-		  *
-		  * Gets the run start time
-		  * @return DateTime The run start time
-		  *
-		  */
+	/**
+	 *
+	 * Gets the run start time
+	 * @return DateTime The run start time
+	 *
+	 */
 	public function getStart() {
 		return $this->start;
 	}
-	
-		 /**
-		  *
-		  * Gets the run distance
-		  * @return double The run distance
-		  *
-		  */
+
+	/**
+	 *
+	 * Gets the run distance
+	 * @return double
+	 *
+	 */
 	public function getDistance() {
 		return $this->distance;
 	}
+	/**
+	 *
+	 * Gets the run distance unit
+	 * @return int
+	 *
+	 */
+	public function getDistanceUnit() {
+		return $this->distanceUnit;
+	}
+
+	/**
+	 *
+	 * Gets the formated run distance unit
+	 * @return string
+	 *
+	 */
+	public function getDistanceUnitFormated() {
+		return Tx_EcDonationrun_ViewHelpers_DistanceFormatViewHelper::render($this->distanceUnit);
+	}
+
 	/**
 	 *
 	 * Gets the event
@@ -146,12 +172,12 @@ class Tx_EcDonationrun_Domain_Model_Run extends Tx_Extbase_DomainObject_Abstract
 	public function getEvent() {
 		return $this->event;
 	}
-		/**
-		 *
-		 * Gets all registrations for this run.
-		 * @return Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Registration> All registrations
-		 *
-		 */
+	/**
+	 *
+	 * Gets all registrations for this run.
+	 * @return Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Registration> All registrations
+	 *
+	 */
 	public function getRegistrations() {
 		if ($this->registrations === NULL) {
 			$this->registrations = New Tx_Extbase_Persistence_ObjectStorage();
@@ -159,65 +185,65 @@ class Tx_EcDonationrun_Domain_Model_Run extends Tx_Extbase_DomainObject_Abstract
 		return $this->registrations;
 	}
 
-		/**
-		 *
-		 * Gets all donations for this run.
-		 * @return Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Donation> All donations
-		 *
-		 */
+	/**
+	 *
+	 * Gets all donations for this run.
+	 * @return Tx_Extbase_Persistence_ObjectStorage<Tx_EcDonationrun_Domain_Model_Donation> All donations
+	 *
+	 */
 	public function getDonations() {
 		if ($this->donations === NULL) {
 			$this->donations = New Tx_Extbase_Persistence_ObjectStorage();
 			ForEach($this->getRegistrations() As $registration)
-				$this->donations->addAll($registration->getDonations());
+			$this->donations->addAll($registration->getDonations());
 		}
 		return $this->donations;
 	}
-	
-		/**
-		 *
-		 * Gets the edit date.
-		 * @return DateTime The edit date
-		 *
-		 */
+
+	/**
+	 *
+	 * Gets the edit date.
+	 * @return DateTime The edit date
+	 *
+	 */
 	public function getEditDate() { return $this->tstamp; }
 
-		/*
-		 * SETTERS
-		 */
-		 /**
-		  *
-		  * Sets the run name
-		  * @param string $name The run name
-		  * @return void
-		  *
-		  */
+	/*
+	 * SETTERS
+	 */
+	/**
+	 *
+	 * Sets the run name
+	 * @param string $name The run name
+	 * @return void
+	 *
+	 */
 	public function setName($name) {
 		$this->name = $name;
 	}
 
-		/**
-		 *
-		 * Sets the start date.
-		 * @param DateTime $start The start date
-		 * @return void
-		 *
-		 */
+	/**
+	 *
+	 * Sets the start date.
+	 * @param DateTime $start The start date
+	 * @return void
+	 *
+	 */
 	public function setStart(DateTime $start) {
 		$this->start = $start;
 	}
-	
-		/**
-		 *
-		 * Sets the distance.
-		 * @param double $distance The run distance
-		 * @return void
-		 *
-		 */
+
+	/**
+	 *
+	 * Sets the distance.
+	 * @param double $distance The run distance
+	 * @return void
+	 *
+	 */
 	public function setDistance($distance) {
 		$this->distance = $distance;
 	}
-	
+
 	/**
 	 *
 	 * Sets the event.
@@ -228,7 +254,7 @@ class Tx_EcDonationrun_Domain_Model_Run extends Tx_Extbase_DomainObject_Abstract
 	public function setEvent(Tx_EcDonationrun_Domain_Model_Event $event) {
 		$this->event = $event;
 	}
-	
+
 	/**
 	 * Has the current fe user already a registration?
 	 * @return boolean
@@ -245,9 +271,9 @@ class Tx_EcDonationrun_Domain_Model_Run extends Tx_Extbase_DomainObject_Abstract
 			}
 		}
 		return $userHasRegistration;
-		
+
 	}
-	
+
 }
 
 ?>
